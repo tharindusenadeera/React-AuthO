064f61c1c9097609d4dae22807f590b6c7b1e559
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-06-13 20:36:06 +0530

    Initial Commit

---
+++ react-auth0/src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+    render() {
+        return ( < p className = "App-intro" >
+            To get started, edit < code > src / App.js < /code> and save to reload. < /
+            p >
+
+        );
+    }
+
+}
